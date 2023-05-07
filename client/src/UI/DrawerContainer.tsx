17d1e0765dfbb31828f864fdb3b70ff64759b88a
nmutavdzic99 <nmutavdzic99@gmail.com>
2023-05-07 12:31:48 +0200

    added styling for active and inactive drawer items

--- client/src/UI/DrawerContainer.tsx
+++ client/src/UI/DrawerContainer.tsx
@@ -1,11 +1,16 @@
 import '../styles/DrawerContainerStyle.css'
 import {faObjectGroup, faSignOut, faTasks, faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {useNavigate} from "react-router-dom";
-import {ScreenNames} from "../common/constants/ScreenNames";
+import {useLocation, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const DrawerContainer = () => {
+    const inactiveDrawerItemClass = 'drawer-item-container inactive';
+    const activeDrawerItemClass = 'drawer-item-container active';
+
     const navigate = useNavigate();
+    const location = useLocation();
+    const [activeDrawerItem, setActiveDrawerItem] = useState('/');
 
     const navigationHandler = (screen: string) => {
         navigate(screen);
@@ -17,21 +22,29 @@ const DrawerContainer = () => {
         navigate('/login');
     }
 
+    const getItemContainerClassName = (path: string) => {
+        return activeDrawerItem === path ? activeDrawerItemClass : inactiveDrawerItemClass;
+    }
+
+    useEffect(() => {
+        setActiveDrawerItem(location.pathname);
+    }, [location.pathname])
+
     return (
         <div className={'drawer-container'}>
             <div className={'drawer-items-container'}>
-                <div className={'drawer-item-container inactive'}
-                     onClick={navigationHandler.bind(this, ScreenNames.usersScreen)}>
+                <div className={getItemContainerClassName('/')}
+                     onClick={navigationHandler.bind(this, '/')}>
                     <FontAwesomeIcon icon={faUser} className={'icon'}/>
                     <p>Users</p>
                 </div>
-                <div className={'drawer-item-container inactive'}
-                     onClick={navigationHandler.bind(this, ScreenNames.tasksScreen)}>
+                <div className={getItemContainerClassName('/tasks')}
+                     onClick={navigationHandler.bind(this, '/tasks')}>
                     <FontAwesomeIcon icon={faTasks} className={'icon'}/>
                     <p>Tasks</p>
                 </div>
-                <div className={'drawer-item-container inactive'}
-                     onClick={navigationHandler.bind(this, ScreenNames.taskGroupsScreen)}>
+                <div className={getItemContainerClassName('/task-groups')}
+                     onClick={navigationHandler.bind(this, 'task-groups')}>
                     <FontAwesomeIcon icon={faObjectGroup} className={'icon'}/>
                     <p>Task Groups</p>
                 </div>
