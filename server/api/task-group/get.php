940f55b7ae0f4fc3ac87944cc49b0c117d7e6ef8
nemanja <d.neca99@gmail.com>
2023-05-15 21:21:48 +0200

    task-groups all and get

---
+++ server/api/task-group/get.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../../src/services/apiController.php';
+require_once __DIR__ . '/../../src/services/user/authService.php';
+require_once __DIR__ . '/../../src/db/Database.php';
+
+checkRequestType('GET');
+if ((true !== $result = canUserAccess('Rukovodilac'))) {
+    unauthorized($result);
+}
+
+if (empty($_GET['id'])) {
+    badRequest('Id is missing!');
+}
+$id = htmlspecialchars($_GET['id']);
+
+try {
+    $db = Database::getConnection();
+
+    $taskGroup = [];
+    $statement = $db->prepare("SELECT * FROM taskGroup WHERE id=:id");
+    $statement->bindParam('id', $id);
+    $statement->execute();
+    if (!$statement || !($taskGroup = $statement->fetch(PDO::FETCH_ASSOC))) {
+        badRequest('Can not get task group with provided id ' . $id);
+    }
+
+    sendResponse(['data' => $taskGroup]);
+} catch (Exception $exception) {
+    badRequest('Error occurred while getting task group with id ' . $id);
+}
