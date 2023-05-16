97c6ac61bb1094c0aa765406472f5e9fc372078c
nemanja <d.neca99@gmail.com>
2023-05-16 19:22:07 +0200

    safety

--- server/src/services/apiController.php
+++ server/src/services/apiController.php
@@ -37,6 +37,21 @@ function unauthorized(string $error)
 
 function checkRequestType(string $type = 'POST')
 {
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        header('Access-Control-Allow-Origin: http://localhost:3000');
+        header('Access-Control-Allow-Methods: POST');
+        header(
+            'Access-Control-Allow-Headers: Content-Type, 
+                    Access-Control-Allow-Origin,
+                    Authorization,
+                    Access-Control-Allow-Methods'
+        );
+        header('Access-Control-Max-Age: 86400'); // 24 hours
+        header('Access-Control-Allow-Credentials: true'); // 24 hours
+        header('HTTP/1.1 200 OK');
+        exit;
+    }
     if ($_SERVER['REQUEST_METHOD'] !== $type) {
         badRequest('No route found!');
     }
